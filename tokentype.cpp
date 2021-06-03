7294e3ed4b8ca83c8bafe95e4f6a0b0f4bd2c8b5
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2021-06-03 13:39:24 -0400

    Create TokenType header file

--- tokentype.cpp
+++ tokentype.cpp
@@ -1,67 +1,46 @@
-#include <iostream>
-
-enum TokenType {
-    // single character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-
-    // one or two character tokens
-    BANG, BANG_EQUAL,
-    EQUAL, EQUAL_EQUAL,
-    GREATER, GREATER_EQUAL,
-    LESS, LESS_EQUAL,
-
-    // literals
-    IDENTIFIER, STRING, NUMBER,
-
-    // keywords
-    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-
-    EF
-};
+#include "tokentype.h"
 
 std::ostream& operator<<(std::ostream& out, const TokenType type) {
     switch (type) {
-        case LEFT_PAREN:    out << "LEFT_PAREN"; break;
-        case RIGHT_PAREN:   out << "RIGHT_PAREN"; break;
-        case LEFT_BRACE:    out << "LEFT_BRACE"; break;
-        case RIGHT_BRACE:   out << "RIGHT_BRACE"; break;
-        case COMMA:         out << "COMMA"; break;
-        case DOT:           out << "DOT"; break;
-        case MINUS:         out << "MINUS"; break;
-        case PLUS:          out << "PLUS"; break;
-        case SEMICOLON:     out << "SEMICOLON"; break;
-        case SLASH:         out << "SLASH"; break;
-        case STAR:          out << "STAR"; break;
-        case BANG:          out << "BANG"; break;
-        case BANG_EQUAL:    out << "BANG_EQUAL"; break;
-        case EQUAL:         out << "EQUAL"; break;
-        case EQUAL_EQUAL:   out << "EQUAL_EQUAL"; break;
-        case GREATER:       out << "GREATER"; break;
-        case GREATER_EQUAL: out << "GREATER_EQUAL"; break;
-        case LESS:          out << "LESS"; break;
-        case LESS_EQUAL:    out << "LESS_EQUAL"; break;
-        case IDENTIFIER:    out << "IDENTIFIER"; break;
-        case STRING:        out << "STRING"; break;
-        case NUMBER:        out << "NUMBER"; break;
-        case AND:           out << "AND"; break;
-        case CLASS:         out << "CLASS"; break;
-        case ELSE:          out << "ELSE"; break;
-        case FALSE:         out << "FALSE"; break;
-        case FUN:           out << "FUN"; break;
-        case FOR:           out << "FOR"; break;
-        case IF:            out << "IF"; break;
-        case NIL:           out << "NIL"; break;
-        case OR:            out << "OR"; break;
-        case PRINT:         out << "PRINT"; break;
-        case RETURN:        out << "RETURN"; break;
-        case SUPER:         out << "SUPER"; break;
-        case THIS:          out << "THIS"; break;
-        case TRUE:          out << "TRUE"; break;
-        case VAR:           out << "VAR"; break;
-        case WHILE:         out << "WHILE"; break;
-        case EF:            out << "EF"; break;
+        case TokenType::LEFT_PAREN:    out << "LEFT_PAREN"; break;
+        case TokenType::RIGHT_PAREN:   out << "RIGHT_PAREN"; break;
+        case TokenType::LEFT_BRACE:    out << "LEFT_BRACE"; break;
+        case TokenType::RIGHT_BRACE:   out << "RIGHT_BRACE"; break;
+        case TokenType::COMMA:         out << "COMMA"; break;
+        case TokenType::DOT:           out << "DOT"; break;
+        case TokenType::MINUS:         out << "MINUS"; break;
+        case TokenType::PLUS:          out << "PLUS"; break;
+        case TokenType::SEMICOLON:     out << "SEMICOLON"; break;
+        case TokenType::SLASH:         out << "SLASH"; break;
+        case TokenType::STAR:          out << "STAR"; break;
+        case TokenType::BANG:          out << "BANG"; break;
+        case TokenType::BANG_EQUAL:    out << "BANG_EQUAL"; break;
+        case TokenType::EQUAL:         out << "EQUAL"; break;
+        case TokenType::EQUAL_EQUAL:   out << "EQUAL_EQUAL"; break;
+        case TokenType::GREATER:       out << "GREATER"; break;
+        case TokenType::GREATER_EQUAL: out << "GREATER_EQUAL"; break;
+        case TokenType::LESS:          out << "LESS"; break;
+        case TokenType::LESS_EQUAL:    out << "LESS_EQUAL"; break;
+        case TokenType::IDENTIFIER:    out << "IDENTIFIER"; break;
+        case TokenType::STRING:        out << "STRING"; break;
+        case TokenType::NUMBER:        out << "NUMBER"; break;
+        case TokenType::AND:           out << "AND"; break;
+        case TokenType::CLASS:         out << "CLASS"; break;
+        case TokenType::ELSE:          out << "ELSE"; break;
+        case TokenType::FALSE:         out << "FALSE"; break;
+        case TokenType::FUN:           out << "FUN"; break;
+        case TokenType::FOR:           out << "FOR"; break;
+        case TokenType::IF:            out << "IF"; break;
+        case TokenType::NIL:           out << "NIL"; break;
+        case TokenType::OR:            out << "OR"; break;
+        case TokenType::PRINT:         out << "PRINT"; break;
+        case TokenType::RETURN:        out << "RETURN"; break;
+        case TokenType::SUPER:         out << "SUPER"; break;
+        case TokenType::THIS:          out << "THIS"; break;
+        case TokenType::TRUE:          out << "TRUE"; break;
+        case TokenType::VAR:           out << "VAR"; break;
+        case TokenType::WHILE:         out << "WHILE"; break;
+        case TokenType::END_FILE:      out << "END_FILE"; break;
     }
     return out;
 }
