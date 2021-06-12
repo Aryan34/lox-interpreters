063944987027a207a8bf9add3e6443a2cb7bd2d8
Aryan34 <aryanc2021@gmail.com>
2021-06-12 13:42:39 -0400

    Create parser header file

---
+++ parser.h
@@ -0,0 +1,35 @@
+#ifndef LOX_INTERPRETERS_PARSER_H
+#define LOX_INTERPRETERS_PARSER_H
+
+#include <vector>
+
+#include "token.h"
+#include "expr.h"
+
+class Parser {
+public:
+    explicit Parser(std::vector<Token> tokens) : tokens(std::move(tokens)) {}
+
+private:
+    const std::vector<Token> tokens;
+    int current = 0;
+
+    std::unique_ptr<Expr> expression();
+    std::unique_ptr<Expr> equality();
+    std::unique_ptr<Expr> comparison();
+    std::unique_ptr<Expr> term();
+    std::unique_ptr<Expr> factor();
+    std::unique_ptr<Expr> unary();
+    std::unique_ptr<Expr> primary();
+
+    bool match(std::initializer_list<TokenType> types);
+    bool check(TokenType type);
+    bool isAtEnd();
+
+    Token advance();
+    Token peek();
+    Token previous();
+    Token consume(TokenType type, std::string message);
+};
+
+#endif //LOX_INTERPRETERS_PARSER_H
